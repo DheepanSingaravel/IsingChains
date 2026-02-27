f3f9d6335166ad0f3f43ff0ff5526b295504917a
Dheepan Singaravel <dheepansingaravel7@gmail.com>
2026-02-27 23:25:16 +0000

    Energy study of alligned vs alternating spin states

--- Ising1D.py
+++ Ising1D.py
@@ -1,3 +1,34 @@
 import numpy as np
+from numpy import random
+import matplotlib.pyplot as plt
 
 # Start of with defining lattice positions in 1D and seperate array for spin states randomised to find total energy.
+
+# Define spin states
+N = 100
+spin_states = np.random.choice([-1, 1], size=N)
+
+# Write function for general expression for hamiltonian H = -J sigma (s1.s2) - h sigma s. Assume a closed form, where the first and final spin states are neighbouring.
+
+def config_energy(list_spins, h):
+    edge_case = list_spins[0]*list_spins[-1]
+    sum_list_interacting = [list_spins[i]*list_spins[i+1] for i in range(0, len(list_spins) - 1)]
+    sum_list_ext = sum(list_spins)
+    return ((-1*(edge_case + sum(sum_list_interacting))) - h*sum_list_ext)
+
+print(config_energy(spin_states, 0))
+
+# Special Case 1 : All spin up
+
+spin_states_up = [1]*N
+
+# Special Case 2 : All spin down
+
+spin_states_down = [-1]*N
+
+# Special Case 1 : Alternating spin up and down
+
+spin_states_alt = [(-1)**n for n in range(N)]
+
+plt.plot(['spin_states_up', 'spin_states_down', 'spin_states_alt'], [config_energy(spin_states_up,0), config_energy(spin_states_down,0), config_energy(spin_states_alt,0)])
+plt.show()
